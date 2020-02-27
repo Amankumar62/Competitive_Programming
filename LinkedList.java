f955b0f301555eff953a332199cf5673bff008ee
Amankumar62 <amankumar12091999@gmail.com>
2020-02-27 22:10:12 +0530

    Added more methods

--- LinkedList.java
+++ LinkedList.java
@@ -34,12 +34,80 @@ public class LinkedList {
 			this.size++;
 		}
 	}
-	public static void main(String A[]){
+	public void addFirst(int item){
+		Node nn = new Node();
+		nn.data = item;
+		nn.next = null;
+		
+		//linking nodes 
+		if(this.size >=1){
+			nn.next = this.head;
+		}
+		
+		//updtaing summary object
+		if(this.size ==0){
+			this.head = nn;
+			this.tail= nn;
+			this.size++;
+		}
+		else{
+			this.head = nn;
+			this.size++;
+		}
+	}
+		
+		public int getAt( int index) throws Exception{
+			if(this.size ==0){
+				throw new Exception("Linked list is Empty");
+			}
+			if(index<0 || index >size){
+				throw new Exception("Invalid Size");
+			}
+			
+			Node temp = this.head;
+			for(int i=1;i<=index;i++){
+				temp = temp.next;
+			}
+			return temp.data;
+		}
+		
+	public int getFirst() throws Exception{
+		if(this.size==0){
+			throw new Exception("Empty Linked list");
+		}
+		return this.head.data;
+	}
+	public int getLast() throws Exception{
+		if(this.size ==0){
+			throw new Exception("Empty Linked list");
+		}
+		return this.tail.data;
+	}
+	
+	private Node getNodeAt(int index) throws Exception{
+		if(this.size ==0){
+			throw new Exception("Empty Linked list");
+		}
+		if(index <0 || index >this.size){
+			throw new Exception("Invalid index");
+		}
+		
+		Node temp = this.head;
+		for(int i=1;i<=index;i++){
+			temp = temp.next;
+		}
+		return temp;
+		
+	}
+	public static void main(String A[]) throws Exception{
 		LinkedList list = new LinkedList();
 		list.addLast(10);
-		list.addLast(20);
+		list.addFirst(20);
 		list.addLast(30);
-		list.addLast(40);
+		list.addFirst(40);
 		list.display();
+		System.out.println(list.getAt(2));
+		System.out.println(list.getFirst());
+		System.out.println(list.getLast());
 	}
 }
