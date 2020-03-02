5d8d9b77afc6a388a03ebe1d968c0dec297211b1
Amankumar62 <amankumar12091999@gmail.com>
2020-03-02 20:56:52 +0530

    optimization

--- LinkedList.java
+++ LinkedList.java
@@ -95,19 +95,123 @@ public class LinkedList {
 		Node temp = this.head;
 		for(int i=1;i<=index;i++){
 			temp = temp.next;
-		}
+		} 
 		return temp;
+	}
+	public int removeFirst() throws Exception{
+		if(this.size ==0){
+			throw new Exception("Empty linked list");
+		}
+		int rv;
+		if(this.size ==1){
+			 rv = this.head.data;
+			this.head = null;
+			this.tail = null;
+		    this.size =0;
+		}
+		else{
+			rv = this.head.data;
+			this.head = this.head.next;
+		}
+		return rv;
+	}
+	public int removeLast() throws Exception{
+		if(this.size ==0){
+			throw new Exception("Empty linked list");
+		}
+		int rv;
+		if(this.size ==1){
+			rv = this.tail.data;
+			this.head = null;
+			this.tail = null;
+			this.size=0;
+		}
+		else{
+			rv = this.tail.data;
+			Node lm1 = this.getNodeAt(this.size -2);
+			this.tail = lm1;
+			this.tail.next = null;
+			this.size --;
+		}
+		return rv;
+	}
+	
+	public int removeAt(int index) throws Exception {
+		if(this.size ==0 ){
+			throw new Exception("Empty linked List");
+		}
+		if(index<0 || index>=this.size){
+			throw new Exception("Invalid index");
+		}
 		
+		if(index ==0){
+			return this.removeFirst();
+		}
+		if(index==this.size-1){
+			return this.removeLast();
+		}
+		Node nm1 = this.getNodeAt(index -1);
+		Node n = nm1.next;
+		int rv = n.data;
+		Node np1 = n.next;
+		nm1 = np1;
+		n.next = null;
+	    this.size--;
+		return rv;
+	}
+	
+	public void reverseData() throws Exception{
+		int left =0;
+		int right = this.size -1;
+		 while(left < right){
+			 Node ln = getNodeAt(left);
+			 Node rn = getNodeAt(right);
+			 
+			 int temp = ln.data;
+			 ln.data = rn.data;
+			 rn.data = temp;
+			 
+			 left++;
+			 right--;
+		 }
+	}
+	
+	public void reversePointer() {
+		Node prev = this.head;
+		Node curr= prev.next;
+		
+		while(curr!=null){
+			Node ahead = curr.next;
+			
+			curr.next = prev;
+			prev = curr;
+			curr = ahead;
+			}
+		Node temp = this.head;
+		this.head = this.tail;
+		this.tail = temp;
+		this.tail.next= null;
+	}
+	public int mid() {
+		Node slow =this.head;
+		Node fast = this.head;
+		
+		while(fast.next != null && fast.next.next !=null){
+			slow=slow.next;
+			fast = fast.next.next;
+		}
+		return slow.data;
 	}
 	public static void main(String A[]) throws Exception{
 		LinkedList list = new LinkedList();
 		list.addLast(10);
-		list.addFirst(20);
+		list.addLast(20);
 		list.addLast(30);
-		list.addFirst(40);
+		list.addLast(40);
 		list.display();
-		System.out.println(list.getAt(2));
-		System.out.println(list.getFirst());
-		System.out.println(list.getLast());
+		System.out.println(list.mid());
+		System.out.println();
+		list.reversePointer();
+		list.display();
 	}
 }
