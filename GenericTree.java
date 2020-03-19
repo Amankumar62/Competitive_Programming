d442133ed61eaaedfa127c8d4a1c977bf8d41a78
Amankumar62 <amankumar12091999@gmail.com>
2020-03-20 00:18:14 +0530

    display added

--- GenericTree.java
+++ GenericTree.java
@@ -17,7 +17,7 @@ public class GenericTree{
 		this.root = takeInput(s,null,0);
 }
 	private Node takeInput(Scanner s , Node parent,int ithchild){
-		if(this.root == null){
+		if(parent == null){
 			System.out.println("Enter the value of the root node : ");
 		}else{
 			System.out.println("Enter the value of"+(ithchild+1)+"th child of "+parent.data+" : ");
@@ -34,6 +34,23 @@ public class GenericTree{
 			node.children.add(child);
 		}
 	return node;
+		
+	}
+ 
+	public void display(){
+		this.display(this.root);
+	}
+	
+	private void display(Node node){
+		String str = node.data+" => ";
+		for(int i=0;i<node.children.size();i++){
+			str= str + node.children.get(i).data + " , ";
+		}
+		str = str + "END";
+		System.out.println(str);
+		for(int i =0;i<node.children.size();i++){
+			this.display(node.children.get(i));
+		}
 		
 	}
 }
