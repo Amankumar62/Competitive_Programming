993ff59b914f2750f34dcf18aff4d48e19da6e00
Amankumar62 <amankumar12091999@gmail.com>
2020-03-25 22:30:59 +0530

    BST

--- BST.java
+++ BST.java
@@ -53,4 +53,60 @@ public class BST {
 		this.display(node.right);
 		}
 	}
+	
+	public boolean find(int item){
+		return find(this.root,item);
+	}
+	
+	private boolean find(Node node,int item){
+		if(node == null){
+			return false;
+		}
+		if(item > node.data){
+			return find(node.right,item);
+		}
+		else if(item < node.data){
+			return find(node.left,item);
+		}
+		else{
+			return true;
+		}
+	}
+	
+	public int max(){
+		return max(this.root);
+	}
+	private int max(Node node){
+		if(node.right == null){
+			return node.data;
+		}
+		
+		return max(node.right);
+	}
+	
+	public void add(int item){
+		add(this.root,item);
+	}
+	
+	private void add(Node node , int item){
+		if(item > node.data){
+			if(node.right == null){
+				Node nn = new Node();
+				nn.data = item;
+			   node.right = nn;
+			}else{
+				add(node.right,item);
+			}
+		}
+		else{
+			if(node.left == null){
+				Node nn = new Node();
+				nn.data= item;
+				node.left = nn;
+			}
+			else{
+				add(node.left,item);
+			}
+		}
+	}
 }
