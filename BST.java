6461fcd7ac77f43c4d694aee1d0644b3f97a8b41
Amankumar62 <amankumar12091999@gmail.com>
2020-03-27 23:04:16 +0530

    BST functions

--- BST.java
+++ BST.java
@@ -109,4 +109,88 @@ public class BST {
 			}
 		}
 	}
+	
+	public void remove(int item){
+		remove(this.root,null,false,item);
+	}
+	private void remove(Node node,Node parent,boolean ilc,int item){
+		if(node == null){
+			return;
+		}
+		if(item > node.data){
+			remove(node.right,node,false,item);
+		}
+		else if(item < node.data){
+			remove(node.left,node,true,item);
+		}
+		else{
+			if(node.left == null && node.right == null){
+				if(ilc){
+					parent.left = null;
+				}
+				else{
+					parent.right = null;
+				}
+			}
+			else if(node.left== null && node.right != null){
+				 if(ilc){
+					 parent.left = node.right;
+				 }
+				 else{
+					 parent.right = node.right;
+				 }
+			}
+			else if(node.left != null && node.right== null){
+				if(ilc){
+					parent.left = node.left; 
+				}
+				else{
+					parent.right = node.left;
+				}
+			}
+			
+			else{
+				int max = max(node.left);
+				node.data = max;
+				
+				remove(node.left,node,true,max);
+			}
+		}
+	}
+	
+	public boolean isBST(){
+		return isBST(this.root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+	}
+	private boolean isBST(Node node,int min,int max){
+		if(node == null){
+			return true;
+		}
+		if(node.data >max || node.data < min){
+			return false;
+		}
+		else if(!isBST(node.left,min,node.data)){
+			return false;
+		}
+		else if(!isBST(node.right,node.data,max)){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
+	
+	public int sumLeaf(){
+		return sumLeaf(this.root);
+	}
+	private int sumLeaf(Node node){
+		if(node == null){
+			return 0;
+		}
+		if(node.right == null && node.left == null){
+			return node.data;
+		}
+		int lsum = sumLeaf(node.left);
+		int rsum = sumLeaf(node.right);
+		return (lsum+rsum);
+	}
 }
--- BinaryTreeClient.java
+++ BinaryTreeClient.java
@@ -6,6 +6,7 @@ public class BinaryTreeClient {
 		int arr[] ={10,20,30,40,50,60,70};
 			BST tree = new BST(arr);
 			tree.display();
+			System.out.println(tree.sumLeaf());
 			
 	}
 
